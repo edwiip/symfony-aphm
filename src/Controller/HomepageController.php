1a60a6144c941336ed87f8fe65be8b845896839a
Erick Duteurtre <erick@MacBook-Pro-de-Erick.local>
2022-11-03 17:34:23 +0100

    utilisation de twig

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,17 +2,45 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Utilisateur;
+use App\Entity\Service;
+use App\Form\UtilisateurFormType;
+use App\Repository\ServiceRepository;
+use App\Repository\UtilisateurRepository;
+use Twig\Environment;
 
 class HomepageController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
-    public function index(): Response
+    private $entityManager;
+    private $twig;
+
+    /**
+     * @param $entityManager
+     * @param $twig
+     */
+    public function __construct(EntityManagerInterface $entityManager, Environment $twig)
     {
-        return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
-        ]);
+        $this->entityManager = $entityManager;
+        $this->twig = $twig;
+    }
+
+
+    #[Route('/', name: 'homepage')]
+    public function index(ServiceRepository $serviceRepository): Response
+    {
+
+        $message = new Utilisateur();
+        $form = $this->createForm(UtilisateurFormType::class, $message);
+
+
+        return new Response($this->twig->render('homepage/index.html.twig', [
+            'services' => $serviceRepository->findAll(),
+            'userForm' => $form->createView(),
+        ]));
     }
 }
--- src/Form/UtilisateurFormType.php
+++ src/Form/UtilisateurFormType.php
@@ -6,20 +6,37 @@ use App\Entity\Utilisateur;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class UtilisateurFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('telephone')
-            ->add('email')
-            ->add('piecejointe')
+            ->add('nom', null, [
+                'label' => "Votre nom",
+                'required' => true
+            ])
+            ->add('prenom', null, [
+                'label' => "Votre prénom",
+                'required' => true
+            ])
+            ->add('telephone', null, [
+                'label' => "voytre numéro de téléphone",
+                'required' => true
+            ])
+            ->add('email', null, [
+                'label' => "Indiquez une adresse email valide",
+                'required' => true
+            ])
+            ->add('piecejointe', null, [
+                'label' => "Ajouter mon CV"
+            ])
             ->add('message')
             ->add('service')
-        ;
+            ->add('submit', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
