cd4347a3de7e333d7b7c0346c165444e519e44ab
Erick Duteurtre <erick@MacBook-Pro-de-Erick.local>
2022-11-03 21:23:14 +0100

    configuration des types de champs pour le formulaire

--- src/Form/UtilisateurFormType.php
+++ src/Form/UtilisateurFormType.php
@@ -3,38 +3,32 @@
 namespace App\Form;
 
 use App\Entity\Utilisateur;
+
+
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class UtilisateurFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom', null, [
-                'label' => "Votre nom",
-                'required' => true
-            ])
-            ->add('prenom', null, [
-                'label' => "Votre prénom",
-                'required' => true
-            ])
-            ->add('telephone', null, [
-                'label' => "voytre numéro de téléphone",
-                'required' => true
-            ])
-            ->add('email', null, [
-                'label' => "Indiquez une adresse email valide",
-                'required' => true
-            ])
+            ->add('nom', TextType::class, ['label' => 'Votre nom de famille', 'required' => true])
+            ->add('prenom', TextType::class, ['label' => "Votre prénom", 'required' => true])
+            ->add('telephone', TelType::class, ['label' => "voytre numéro de téléphone", 'required' => true])
+            ->add('email', EmailType::class, ['label' => "Indiquez une adresse email valide", 'required' => true])
             ->add('piecejointe', null, [
                 'label' => "Ajouter mon CV"
             ])
-            ->add('message')
+            ->add('message', TextareaType::class)
             ->add('service')
             ->add('submit', SubmitType::class, [
                 'label' => "envoyer"
