e2690313507f72f0cb467db9d8afb6a703b34b08
Erick Duteurtre <erick@MacBook-Pro-de-Erick.local>
2022-11-03 17:57:36 +0100

    création du formulaire

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -3,15 +3,14 @@
 namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
-use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Utilisateur;
 use App\Entity\Service;
 use App\Form\UtilisateurFormType;
 use App\Repository\ServiceRepository;
-use App\Repository\UtilisateurRepository;
 use Twig\Environment;
 
 class HomepageController extends AbstractController
@@ -31,12 +30,18 @@ class HomepageController extends AbstractController
 
 
     #[Route('/', name: 'homepage')]
-    public function index(ServiceRepository $serviceRepository): Response
+    public function index(ServiceRepository $serviceRepository, Request $request): Response
     {
 
         $message = new Utilisateur();
         $form = $this->createForm(UtilisateurFormType::class, $message);
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $this->entityManager->persist($message);
+            $this->entityManager->flush();
+        }
 
         return new Response($this->twig->render('homepage/index.html.twig', [
             'services' => $serviceRepository->findAll(),
