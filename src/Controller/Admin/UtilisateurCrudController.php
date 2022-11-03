658a549c9277b08cfed3fb340e2a2e2cd2e11b4e
Erick Duteurtre <erick@MacBook-Pro-de-Erick.local>
2022-11-03 16:41:38 +0100

    configuration des champs sur les cruds controller

--- src/Controller/Admin/UtilisateurCrudController.php
+++ src/Controller/Admin/UtilisateurCrudController.php
@@ -4,6 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\Utilisateur;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TelephoneField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UtilisateurCrudController extends AbstractCrudController
 {
@@ -12,14 +17,14 @@ class UtilisateurCrudController extends AbstractCrudController
         return Utilisateur::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield AssociationField::new("service");
+        yield TextField::new("nom");
+        yield TextField::new('prenom');
+        yield TelephoneField::new("telephone");
+        yield EmailField::new("email");
+        yield TextField::new("piecejointe");
+        yield TextareaField::new("message");
     }
-    */
 }
